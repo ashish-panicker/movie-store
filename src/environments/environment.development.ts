f9f31b7c9ba8ce9e13776ee9e16164fa6590a5a5
ashish-panicker <ashish.s.panicker@gmail.com>
2024-03-26 10:11:24 +0530

    Initial setup complete - deps included [bootstrap, firebase] - environment config for TMDB API

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  API_KEY: 'bbaa256f715f217d18015398d7f73549',
+  API_READ_TOKEN:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmFhMjU2ZjcxNWYyMTdkMTgwMTUzOThkN2Y3MzU0OSIsInN1YiI6IjYyOTk4YzNhZDRjYzhlMDBhNWUzYjBiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.smkzeYHJ2v78Qz3uzbQzh_mGGDCbun3k8TJNknAUmTY',
+  API_URL: '',
+};
