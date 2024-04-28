d04a3cbcfadd60c2722e8bb6e2cb846e5ff98a28
ashish-panicker <ashish.s.panicker@gmail.com>
2024-04-28 20:55:23 +0530

    rest-calls removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,15 @@
-export const environment = {};
+export const environment = {
+  API_KEY: 'bbaa256f715f217d18015398d7f73549',
+  API_READ_TOKEN:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmFhMjU2ZjcxNWYyMTdkMTgwMTUzOThkN2Y3MzU0OSIsInN1YiI6IjYyOTk4YzNhZDRjYzhlMDBhNWUzYjBiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.smkzeYHJ2v78Qz3uzbQzh_mGGDCbun3k8TJNknAUmTY',
+  API_URL: 'https://api.themoviedb.org/3',
+  IMG_URL: 'https://image.tmdb.org/t/p/',
+  firebaseConfig: {
+    apiKey: 'AIzaSyCPoeWt3BlgB-pspCjOOrKFIUqpjc3I2a4',
+    authDomain: 'movie-store-ce1ac.firebaseapp.com',
+    projectId: 'movie-store-ce1ac',
+    storageBucket: 'movie-store-ce1ac.appspot.com',
+    messagingSenderId: '883539594256',
+    appId: '1:883539594256:web:bbb5f688c5069ef5b9ce95',
+  },
+};
